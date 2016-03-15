19087ef995b435655b06d6a79584c906c0cf5684
Роман Клименко <klimenko@reestr.cic.local>
2016-03-15 10:31:23 +0200

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,33 @@
+package com.company;
+
+public class Main {
+    static byte b =5;
+	static short sh = 8;
+	static int it =99;
+	static long l =189;
+
+	static float f1 =2.6f;
+	static double db = 2.6;
+
+	static boolean bool =true;
+	static boolean bool2 = false;
+
+	static String st = "hello";
+	static char c ='d';
+
+
+    public static void main(String[] args) {
+	System.out.println("byte"+b);
+	System.out.println("short"+sh);
+	System.out.println("int"+it);
+	System.out.println("long"+l);
+	System.out.println("float"+f1);
+	System.out.println("double"+db);
+	System.out.println("bool"+bool);
+	System.out.println("bool2"+bool2);
+
+		System.out.println(st);
+		System.out.println(c);
+
+    }
+}
